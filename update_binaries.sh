9dc26c5c966c82eb2d30e678656f421169560ac7
Hung Dang <hdang@athenahealth.com>
2019-01-25 12:58:22 -0500

    Support different build mode i.e avx2 and sse2

--- update_binaries.sh
+++ update_binaries.sh
@@ -3,39 +3,60 @@ working_dir="${PWD}/../"
 echo $working_dir
 set -eux
 
-# Install fgrep
-pushd "$working_dir/fastgrep/commands"
-cmake ./ > /dev/null
-make clean > /dev/null
-make -j5 install > /dev/null
-popd
+install_binaries() {
+    option=$1
+    if [ $option == "sse2" ]; then
+        cmake_options="-DUSE_AVX2=FALSE"
+    else
+        cmake_options="-DUSE_AVX2=TRUE"
+    fi
+    echo $option
+    echo $cmake_options
 
-# Install mlocate, mfind, mupdatedb, and mwc
-pushd "$working_dir/ioutils/commands"
-cmake ./ > /dev/null
-make clean > /dev/null 
-make -j5 install > /dev/null
-popd
+    # Install fgrep
+    pushd "$working_dir/fastgrep/commands"
+    cmake ./ "$cmake_options" > /dev/null
+    make clean > /dev/null
+    make -j5 install > /dev/null
+    popd
 
-# Install codesearch and source2tests
-pushd "$working_dir/codesearch/commands"
-cmake ./ > /dev/null
-make clean > /dev/null 
-make -j5 install > /dev/null
-popd
+    # Install mlocate, mfind, mupdatedb, and mwc
+    pushd "$working_dir/ioutils/commands"
+    cmake ./ "$cmake_options" > /dev/null
+    make clean > /dev/null 
+    make -j5 install > /dev/null
+    popd
+
+    # # Install codesearch and source2tests
+    # pushd "$working_dir/codesearch/commands"
+    # cmake ./ "$cmake_options" > /dev/null
+    # make clean > /dev/null 
+    # make -j5 install > /dev/null
+    # popd
+}
 
 # Copy binaries to the destination
-platform=`uname -s`
-architect=`uname -m`
-release=`uname -r`
+copy_binaries() {
+    build_mode=$1
+    platform=`uname -s`
+    architect=`uname -m`
+    release=`uname -r`
+    src_dir="$HOME/bin"
+    dest_dir="$platform/$architect/$release/$build_mode"
+    mkdir -p $dest_dir
+    binaries="mwc fgrep mlocate mupdatedb mfind codesearch source2tests "
+    for cmd in $binaries; do
+        echo "Copying $cmd ..."
+        cp -f "$src_dir/$cmd" "$dest_dir/$cmd"
+    done
+}
 
-src_dir="$HOME/bin"
-dest_dir="$platform/$architect/$release"
-mkdir -p $dest_dir
-binaries="fgrep mlocate mupdatedb mfind codesearch source2tests "
-for cmd in $binaries; do
-    echo "Copying $cmd ..."
-    cp -f "$src_dir/$cmd" "$dest_dir/$cmd"
-done
+# Build with sse2 support
+install_binaries sse2
+copy_binaries sse2
 
-set +x
+# Build with avx2 support.
+install_binaries avx2
+copy_binaries avx2
+
+# set +x
