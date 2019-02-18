0443275775a62f43d9c9561c236ab7a8108642f3
Hung Dang <hdang@athenahealth.com>
2019-02-18 11:22:43 -0500

    Add a script which can add the given binary folder to the system path.
    Former-commit-id: fcebd129e20a43a19dab507fee06e14278bd5b1d [formerly 60aebf245b4a4469f76d3f450759e990d915c970]
    Former-commit-id: 03a26af591c6e02191e14692932d73965932909e

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin.bash
+set -eux
+binary_folder=$1
+
+# Add the given folder to the system search path.
+pushd $binary_folder
+export PATH="$binary_folder:$PATH"
+echo "$binary_folder has been added to the system search paths."
+popd
